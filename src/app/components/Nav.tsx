b187c305a4a2e7692374b9a87449412d6d408c16
asnaweevixvify <asnaweeezor@gmail.com>
2025-09-18 22:51:39 +0700

    add login file

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -1,8 +1,8 @@
-"use client";
-import Image from "next/image";
-import { GiHamburgerMenu } from "react-icons/gi";
-import { useState } from "react";
-import Link from "next/link";
+'use client';
+import Image from 'next/image';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { useState } from 'react';
+import Link from 'next/link';
 
 export default function Nav() {
   const [open, isOpen] = useState(false);
@@ -11,33 +11,33 @@ export default function Nav() {
     <div>
       <nav className="relative">
         <GiHamburgerMenu
-          className="lg:hidden absolute top-10 left-5 z-50 w-5 h-5"
-          style={{ color: open ? "black" : "white" }}
+          className="absolute top-10 left-5 z-50 h-5 w-5 lg:hidden"
+          style={{ color: open ? 'black' : 'white' }}
           onClick={() => isOpen(!open)}
         />
         <ul
-          className={`flex flex-col lg:flex-row gap-10 lg:gap-0 lg:fixed absolute pt-15 lg:pt-0 lg:justify-around lg:bg-transparent bg-white items-center top-0 left-0 transform ${
-            open ? "translate-x-0" : "-translate-x-full"
-          } lg:left-[50%] lg:transform lg:-translate-x-1/2 lg:w-[50%] w-[35vw] lg:h-[150px] h-[100vh] lg:text-white lg:text-2xl transition-transform duration-500 ease-in-out lg:transition-none`}
+          className={`absolute top-0 left-0 flex transform flex-col items-center gap-10 bg-white pt-15 lg:fixed lg:flex-row lg:justify-around lg:gap-0 lg:bg-transparent lg:pt-0 ${
+            open ? 'translate-x-0' : '-translate-x-full'
+          } h-[100vh] w-[35vw] transition-transform duration-500 ease-in-out lg:left-[50%] lg:h-[150px] lg:w-[50%] lg:-translate-x-1/2 lg:transform lg:text-2xl lg:text-white lg:transition-none`}
         >
-          <Link href={"/"}>
+          <Link href={'/'}>
             <li>
               <Image
                 src="/calc.png"
                 width={150}
                 height={150}
                 alt="logo"
-                className="lg:w-[150px] lg:h-[150px] w-0 h-0"
+                className="h-0 w-0 lg:h-[150px] lg:w-[150px]"
               ></Image>
             </li>
           </Link>
           <li>รายละเอียด</li>
-          <Link href={"/qualifications"}>
+          <Link href={'/qualifications'}>
             <li>คุณสมบัติ</li>
           </Link>
           <li>Timelines</li>
           <li>สถานที่</li>
-          <li className="text-[#101557] bg-white p-3 rounded-2xl">ลงทะเบียน</li>
+          <li className="rounded-2xl bg-white p-3 text-[#101557]">ลงทะเบียน</li>
         </ul>
       </nav>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,31 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-[100vh] w-[100vw]">
-      <div className="flex justify-center items-start lg:gap-30">
-        <div className="flex flex-col justify-center items-start gap-5 lg:pt-20">
-          <div className="flex justify-center items-center lg:gap-3">
-            <h1 className="lg:text-4xl font-bold text-white">
+    <div className="flex h-[100vh] w-[100vw] items-center justify-center">
+      <div className="flex items-start justify-center lg:gap-30">
+        <div className="flex flex-col items-start justify-center gap-5 lg:pt-20">
+          <div className="flex items-center justify-center lg:gap-3">
+            <h1 className="font-bold text-white lg:text-4xl">
               CALCTECH CAMP X
             </h1>
-            <h1 className="lg:text-4xl font-bold text-[#101557] bg-white pl-3 pr-3 pt-1 pb-1 rounded-2xl">
+            <h1 className="rounded-2xl bg-white pt-1 pr-3 pb-1 pl-3 font-bold text-[#101557] lg:text-4xl">
               CASIO
             </h1>
-            <h1 className="lg:text-4xl font-bold text-white">คืออะไร ?</h1>
+            <h1 className="font-bold text-white lg:text-4xl">คืออะไร ?</h1>
           </div>
-          <p className="text-white lg:text-2xl lg:max-w-[500px] max-w-[200px] text-left ">
+          <p className="max-w-[200px] text-left text-white lg:max-w-[500px] lg:text-2xl">
             คือค่ายการแข่งขันนวัตกรรมตามหลัก SDGs
             ที่ผู้เข้าแข่งขันจะต้องนำความรู้เกี่ยวกับเครื่องคิดเลขมาประยุกต์ใช้กับนวัตกรรม
           </p>
         </div>
         <div>
           <Image
-            src={"/เครื่องคิดเลข.png"}
+            src={'/เครื่องคิดเลข.png'}
             width={300}
             height={300}
             alt="คุณสมบัติ"
-            className="rounded-2xl lg:w-[300px] lg:h-[500px] w-[150px] h-[250px]"
+            className="h-[250px] w-[150px] rounded-2xl lg:h-[500px] lg:w-[300px]"
           ></Image>
         </div>
       </div>
