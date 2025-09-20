312d0100b7a2587a0d1cc35351ecc697124da0af
Meow-nah-ngo <thitaree1811@gmail.com>
2025-09-20 11:08:58 +0700

    อัปไว้ก่อน

--- src/app/components/timeline.tsx
+++ src/app/components/timeline.tsx
@@ -6,8 +6,11 @@ export default function Timeline() {
 
       <div className="flex flex-col gap-50">
             <div className="relative">
-              <div className="w-10 h-10 rounded-full bg-amber-300"></div>
+              <div className="flex items-center justify-center w-10 h-10 rounded-full bg-transparent backdrop-blur-lg shadow-blue-700">
+                <span className="w-4 h-4 bg-white rounded-full"></span>
+              </div>
             </div>
+
             <div className="text-white">
               <h2 className="text-xl font-bold">กิจกรรม</h2>
               <p className="text-gray-300">วันที่xxx</p>
