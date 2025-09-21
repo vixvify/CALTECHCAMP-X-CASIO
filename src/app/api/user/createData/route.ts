e73b0f6e7abde0748b7003f4d647ee0a3282dc49
asnaweevixvify <asnaweeezor@gmail.com>
2025-09-21 21:19:10 +0700

    no token no api (createData)

--- src/app/api/user/createData/route.ts
+++ src/app/api/user/createData/route.ts
@@ -2,20 +2,23 @@ import { NextResponse } from 'next/dist/server/web/spec-extension/response';
 import { NextRequest } from 'next/dist/server/web/spec-extension/request';
 import { PrismaClient } from '@/generated/prisma';
 import bcrypt from 'bcryptjs';
+import { getToken } from 'next-auth/jwt';
 
 export async function POST(req: NextRequest) {
   const prisma = new PrismaClient();
   const data = await req.json();
+  const secret = process.env.NEXTAUTH_SECRET;
+  const token = await getToken({ req, secret });
 
-  if (!data) {
-    return NextResponse.json({ msg: 'กรุณากรอกข้อมูลให้ครบ' }, { status: 400 });
+  if (!token) {
+    return NextResponse.json({ msg: 'กรุณาเข้าสู่ระบบ' }, { status: 401 });
   }
   try {
     const { password } = data;
     const newPass = await bcrypt.hash(password, 10);
     const newData = { ...data, password: newPass };
     await prisma.user.create({ data: newData });
-    return NextResponse.json({ msg: 'สร้างบัญชีสำเร็จ' }, { status: 201 });
+    return NextResponse.json({ msg: 'สมัครค่ายสำเร็จ' }, { status: 201 });
   } catch (err) {
     return NextResponse.json({ error: err }, { status: 400 });
   }
