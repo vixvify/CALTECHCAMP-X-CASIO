c785769278e74664336ff7eba30024105b0ef9a5
Meow-nah-ngo <thitaree1811@gmail.com>
2025-09-23 10:22:04 +0700

    แก้แผนที่

--- src/app/components/place.tsx
+++ src/app/components/place.tsx
@@ -9,8 +9,9 @@ export default function Place() {
         <div>
             <iframe 
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1938.5345233346632!2d100.4925656318665!3d13.653563775652513!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e2a24d8103a75f%3A0xac86a3dc0e1f8444!2sScience%20Learning%20Space!5e0!3m2!1sth!2sth!4v1758430067258!5m2!1sth!2sth" 
-            width="800" 
-            height="600" 
+            className="mx-auto mb-20 w-[90vw] h-[300px]
+            md:w-[600px] md:h-[400px]
+            lg:w-[800px] lg:h-[500px]"
             style={{border:0}} 
             loading="lazy" 
             />
