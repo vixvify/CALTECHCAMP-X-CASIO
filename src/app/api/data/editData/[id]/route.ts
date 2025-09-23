b7ef1e42e99a5759ec66a308f96577fcfd72de74
asnaweevixvify <asnaweeezor@gmail.com>
2025-09-23 08:33:00 +0700

    Full CRUD System

--- src/app/api/data/editData/[id]/route.ts
+++ src/app/api/data/editData/[id]/route.ts
@@ -1,8 +1,22 @@
 import { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 import { PrismaClient } from '@/generated/prisma';
+import bcrypt from 'bcryptjs';
 
 export async function PUT(req: NextRequest, { params }: { params: any }) {
   const prisma = new PrismaClient();
   const { id } = params;
+  const data = await req.json();
+
+  delete data['id'];
+  const { password } = data;
+  const newPass = await bcrypt.hash(password, 10);
+  const newData = { ...data, password: newPass };
+
+  try {
+    await prisma.user.update({ where: { id }, data: newData });
+    return NextResponse.json({ msg: 'แก้ไขสำเร็จ' }, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({ error: err }, { status: 400 });
+  }
 }
--- src/app/api/data/updateStats/[id]/route.ts
+++ src/app/api/data/updateStats/[id]/route.ts
@@ -5,4 +5,17 @@ import { PrismaClient } from '@/generated/prisma';
 export async function PATCH(req: NextRequest, { params }: { params: any }) {
   const prisma = new PrismaClient();
   const { id } = params;
+  const data = await req.json();
+
+  try {
+    await prisma.user.update({ where: { id }, data });
+    return NextResponse.json({ status: 201 });
+  } catch (err) {
+    return NextResponse.json(
+      {
+        error: err,
+      },
+      { status: 500 },
+    );
+  }
 }
