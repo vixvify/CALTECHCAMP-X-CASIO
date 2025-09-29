6fe709c6782f39b18a66defd5b71d9e7929e90ce
asnaweevixvify <asnaweeezor@gmail.com>
2025-09-29 17:51:01 +0700

    test 2

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,7 +1,8 @@
 import { NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
+import { NextRequest } from 'next/server';
 
-export async function GET() {
+export async function GET(req: NextRequest) {
   const data = await prisma.user.findMany();
   return NextResponse.json(data);
 }
