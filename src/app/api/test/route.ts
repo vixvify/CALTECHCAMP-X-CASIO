80c873a30919095703202f310fcff87ac828f6b9
asnaweevixvify <asnaweeezor@gmail.com>
2025-09-29 17:58:22 +0700

    test 3

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -3,6 +3,10 @@ import { prisma } from '@/lib/prisma';
 import { NextRequest } from 'next/server';
 
 export async function GET(req: NextRequest) {
-  const data = await prisma.user.findMany();
-  return NextResponse.json(data);
+  try {
+    const data = await prisma.user.findMany();
+    return NextResponse.json(data, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({ error: err }, { status: 404 });
+  }
 }
